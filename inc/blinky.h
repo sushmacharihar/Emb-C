f9ed40f218e2a52da7a49ac6e754f1d545cb84f8
Sushma harihar <67408334+sushmacharihar@users.noreply.github.com>
2021-04-24 14:57:00 +0530

    Add files via upload

---
+++ inc/blinky.h
@@ -0,0 +1,40 @@
+#ifndef __BLINKY_H_
+#define __BLINKY_H_
+/**
+ * @file blinky.h
+ * @author Sushma Harihar(sushmaharihar1839@gmail.com)
+ * @brief Project to Blink an LED only when heat button is on and passenger is seated connected to AVR328 MCU GPIO Pin
+ * @version 0.1
+ * @date 2021-04-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+/**
+ * Macro Definitions
+ */
+#define LED_ON 	(0x01)			/**< LED state HIGH */
+#define LED_OFF	(0x00)			/**< LED state LOW */
+
+#define SWITCH_PRESS       /**< When switch is pressed then LED is on  */
+#define SWITCH_RELEASE     /**< When switch is released then LED is off */
+
+/**
+ * Include files
+ */ 
+#include <avr/io.h>
+
+/**
+ * Function Definitions
+ */
+
+/**
+ * @brief Change the state of the LED Pin according to the value of state
+ * 
+ * @param state Pin level to which the LED Pin should be set
+ */
+void change_led_state(uint8_t state);
+
+
+#endif /** __BLINKY_H_ */
---
+++ inc/project_config.h
@@ -0,0 +1,25 @@
+#ifndef __PROJECT_CONFIG_H__
+#define __PROJECT_CONFIG_H__
+/**
+ * @file project_config.h
+ * @author Sushma harihar (sushmaharihar1839@gmail.com)
+ * @brief Configuration file to define pins and ports for the interfaced peripherals
+ * @version 0.1
+ * @date 2021-04-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+/**
+ * Macro Definitions
+ */
+
+#define LED_PORT (PORTB)    /**< LED Port Number */
+#define LED_PIN  (PORTB0)   /**< LED Pin number  */
+
+#define SWITCH_PORT (PORTD)    /**< SWITCH Port Number */
+#define SWITCH_PIN (PORTD0)    /**< SWITCH Pin Number */
+
+
+#endif /* __PROJECT_CONFIG_H__ */
