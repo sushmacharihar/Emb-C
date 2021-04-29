55062bdf9d9bebdcc8829ce1e8a7ae28f81e6da0
Sushma harihar <67408334+sushmacharihar@users.noreply.github.com>
2021-04-30 01:34:24 +0530

    Update and rename project_config.h to Activity2.h

---
+++ inc/Activity2.h
@@ -0,0 +1,29 @@
+#ifndef __Activity2_H__
+#define __Activity2_H__
+/**
+ * @file Activity2.h
+ * @author Sushma harihar (sushmaharihar1839@gmail.com)
+ * @brief Configuration file to define pins and ports for the interfaced peripherals
+ * @version 0.1
+ * @date 2021-04-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#define F_CPU 16000000UL
+
+/**
+ * @brief Configuration init function initializes the values for ports
+ * 
+ */
+
+void Init()
+  
+/**
+ * @brief Configuration this helps in conversion
+ * 
+ */
+  
+  unit8_t ReadADC(unit8_t ch)
+
+#endif /* __Activity2_H__ */
