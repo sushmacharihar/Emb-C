dfac4c9f1138baeff4ce91d3212595a46692a6dd
Sushma harihar <67408334+sushmacharihar@users.noreply.github.com>
2021-04-30 01:12:41 +0530

    Create Activity3.c

---
+++ src/Activity3.c
@@ -0,0 +1,38 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include "Activity3.h"
+
+unsigned volatile temp1;
+void Timer()
+{
+TCCR1A|=(1<<COM1A1)|(1<<WGM11)|(1<<WGM10);
+TCCR1B|=(1<<WGM12)|(1<<CS11)|(1<<CS10);
+
+DDRB|=(1<<PB1);
+}
+int main3()
+{
+Timer();
+
+    if (OCR1A>=0 && OCR1A<=200)
+    {
+        temp1=20;
+        activity4(temp1);
+    }
+    else if(OCR1A>210 && OCR1A<500)
+    {
+        temp1=25;
+        activity4(temp1);
+    }
+    else if(OCR1A>510 && OCR1A<700)
+    {
+        temp1=29;
+        activity4(temp1);
+    }
+    else if(ADCregister>710 && ADCregister<1024)
+    {
+        temp1=33;
+        activity4(temp1);
+    }
+    return 0;
+}
