5ba74b3515b1fa223d1e64326b197e75d826f8bf
Sushma harihar <67408334+sushmacharihar@users.noreply.github.com>
2021-04-30 01:38:53 +0530

    Update and rename user_utils.h to Activity3.h

---
+++ inc/Activity3.h
@@ -0,0 +1,22 @@
+#ifndef __Activity3_H__
+#define __Activity3_H__
+/**
+ * @file Activity3.h
+ * @author Sushma harihar (sushmaharihar1839@gmail.com)
+ * @brief User defined utilities used across the project
+ * @version 0.1
+ * @date 2021-04-29
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <util/delay.h>
+
+/**
+ * @brief Function to set value
+ * 
+ * 
+ */
+void Init();
+
+#endif /* __Activity3_H__ */
