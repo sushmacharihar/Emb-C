6e7160d5e4d12474b96a49518dc97410cb606102
Sushma harihar <67408334+sushmacharihar@users.noreply.github.com>
2021-04-30 01:26:09 +0530

    Update Activity1.c

--- src/Activity1.c
+++ src/Activity1.c
@@ -1,6 +1,7 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include "Activity1.h"
+
 void init()
 {
 DDRB|=(1<<PB0); //Set B0=1 for LED
@@ -9,18 +10,22 @@ DDRD&=~(1<<PD0); //Clear Bit
 PORTD|=(1<<PD0); //Set Bit
 }
 
+void change_led_state(uint8_t state)
+{
+    PORTB|=(state<<PB0);
+}
+
 int main1(){
     init();
     while(1)
     {
         if(!(PIND&(1<<PD0))) //switch press
         {
-            PORTB|=(1<<PB0); //LED on
+            change_led_state(LED_ON) //LED on
         }
         else
         {
-            PORTB&=~(1<<PB0); //LED off
-            _delay_ms(200);
+            change_led_state(LED_OFF) //LED off
         }
 
     }
