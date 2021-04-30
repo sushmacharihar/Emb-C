670682a1741689e124a379baed1ceda123e41713
Sushma harihar <67408334+sushmacharihar@users.noreply.github.com>
2021-04-30 13:51:34 +0530

    Update Activity2.c

--- src/Activity2.c
+++ src/Activity2.c
@@ -11,12 +11,14 @@ void Count()
 
 uint16_t ReadADC(uint8_t ch)
 {
-
+    //channel selection
     ADMUX&=0xf8;
     ch=ch&0b00000111;
     ADMUX|=ch;
+    //coversion start
     ADCSRA|=(1<<ADSC);
     while(!(ADCSRA & (1<<ADIF)));
+    //clear ADIF
     ADCSRA|=(1<<ADIF);
     return(ADC);
 }
