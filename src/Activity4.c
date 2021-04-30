8e07b2eee87f751554dd64ed3b69e6b516a58a12
Sushma harihar <67408334+sushmacharihar@users.noreply.github.com>
2021-04-30 13:57:10 +0530

    Update Activity4.c

--- src/Activity4.c
+++ src/Activity4.c
@@ -4,11 +4,12 @@
 
 void USARTInit(uint16_t value)
 {
+    //Set Baud rate    
     UBRR0L = value;
     UBRR0H = (value>>8)&0x00ff;
     UCSR0C = (1<<UMSEL00)|(1<<UCSZ01)|(1<<UCSZ00);
 
-
+    //Enable the Receiver and Transmitter
     UCSR0B = (1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<TXCIE0);
 
 }
@@ -19,7 +20,7 @@ char USARTReadChar()
 
     while(!(UCSR0A & (1<<RXC0)))
     {
-
+        //Nothing
     }
 
     return UDR0;
@@ -31,7 +32,7 @@ void USARTWriteChar(char data)
 
     while(!(UCSR0A & (1<<UDRE0)))
     {
-
+        //Nothing
     }
 
     UDR0 = data;
